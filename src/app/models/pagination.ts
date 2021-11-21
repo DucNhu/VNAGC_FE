5794bf6c0a1445893ca9044273c10ca10379c4ef
Nguyễn Văn Thuận (Aptech HN) <thuannvth1909002@fpt.edu.vn>
2021-11-21 09:48:35 +0700

    src\app\models\pagination.ts

---
+++ src/app/models/pagination.ts
@@ -0,0 +1,9 @@
+export class Data {
+    "PageCount": number;
+    "TotalItemCount": number;
+    "PageIndex": number;
+    "PageSize": number;
+    "HasPreviousPage": boolean;
+    "HasNextPage": boolean;
+    "Items": Array<object>;
+}
